e93f2ac939198b9e0adef2b27e6a7e3e27c367f9
Леонид Шубин <lenyashubin2002@gmail.com>
2022-09-20 12:17:51 +0300

    Added Id property

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -10,6 +10,10 @@ namespace TrackerLibrary
     public class PrizeModel
     {
         /// <summary>
+        /// The unique identifier for the prize
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// The numeric identifier for the place.
         /// </summary>
         public int PlaceNumber { get; set; }
