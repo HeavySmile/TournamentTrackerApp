706f4c94276c911e321504933912bcd7ae14d0f8
Леонид Шубин <lenyashubin2002@gmail.com>
2022-09-20 14:18:47 +0300

    Added GlobalConfig class for storing database connections

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - Set up the SQL connector properly
+                SqlConnector sql = new SqlConnector();
+                Connections.Add(sql);
+            }
+            
+            if(textFiles)
+            {
+                // TODO - Create the textfile connection
+                TextConnector text = new TextConnector();
+                Connections.Add(text);
+            }
+        }
+    }
+}
